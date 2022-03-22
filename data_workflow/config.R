98d5797fefe7eb45fa08ebd15a97f50730791f76
philbowsher <indyprb@gmail.com>
2022-03-22 18:10:40 +0000

    add data and gt

---
+++ data_workflow/config.R
@@ -0,0 +1,10 @@
+# config.R
+#   Same idea as an autoexec script. Sets paths to data libraries and makes
+#   general settings necessary throughout code
+
+# Set data library paths ----
+sdtm_lib <- "~/CDISC_pilot_replication/data/sdtm"
+adam_lib <- "~/CDISC_pilot_replication/data/adam"
+
+# Reset the col_name behavior to pre huxtable v5 behavior
+options(huxtable.add_colnames = FALSE)
